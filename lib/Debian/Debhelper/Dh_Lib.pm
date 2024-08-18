MichalMaruska/debhelper

commit a16a181f2f141c8ae6b8101ee530fff7a67cfc78

Change `pkgfile` lookup to have opt-in features rather than always-on [c14]
Previously, `pkgfile` would always allow configuration files to
have name segments or architecture restrictions. This is fine
for files, where the tool can reasonably be expected to work
on multiple files like `dh_installsystemd`.

However, for tools like `dh_install` it seems absurd to have
`d/pkg.foo.install.amd64` with a `dh_install --name` call. With this
commit, tools can now declare their file to be "named" or
"allow-architecture-restriction" if these features are relevant.

lib/Debian/Debhelper/Dh_Lib.pm
@@ -1068,7 +1068,46 @@ sub default_sourcedir {
 	sub pkgfile {
 		# NB: $nameless_variant_handling is an implementation-detail; third-party packages
 		# should not rely on it.
-		my ($package, $filename, $nameless_variant_handling) = @_;
+		my ($opts, $package, $filename);
+		my ($nameless_variant_handling, $named, $support_architecture_restriction, $is_bulk_check);
+
+		# !!NOT A PART OF THE PUBLIC API!!
+		# Bulk test used by dh to speed up the can_skip check.   It
+		# is NOT useful for finding the most precise pkgfile.
+
+		if (ref($_[0]) eq 'HASH') {
+			($opts, $package, $filename) = @_;
+			$is_bulk_check = ref($package) eq 'ARRAY';
+			if ($is_bulk_check) {
+				# If `dh` does not have declarative hints to go by, then it must assume all
+				# variants are possible
+				$named = 1;
+				$support_architecture_restriction = 1;
+			}
+
+			$nameless_variant_handling = $opts->{'internal-nameless-variant-handling'}
+				if exists($opts->{'internal-nameless-variant-handling'});
+			$named = $opts->{'named'} if exists($opts->{'named'});
+			$support_architecture_restriction = $opts->{'support-architecture-restriction'}
+				if exists($opts->{'support-architecture-restriction'});
+		} else {
+			($package, $filename) = @_;
+
+			$is_bulk_check = ref($package) eq 'ARRAY';
+			if ($is_bulk_check) {
+				# If `dh` does not have declarative hints to go by, then it must assume all
+				# variants are possible
+				$named = 1;
+				$support_architecture_restriction = 1;
+			}
+		}
+
+		if (compat(13)) {
+			# Before compat 14, these were unconditionally on.
+			$named = 1;
+			$support_architecture_restriction = 1;
+		}
+
 		my (@try, $check_expensive);
 
 		if (not exists($_check_expensive{$filename})) {
@@ -1093,11 +1132,11 @@ sub default_sourcedir {
 		# globally or not.  But if someone is being "clever" then the
 		# cache is reusable and for the general/normal case, it has no
 		# adverse effects.
-		if (defined $dh{NAME}) {
+		if (defined $dh{NAME} and $opts->{'named'}) {
 			$filename="$dh{NAME}.$filename";
 		}
 
-		if (ref($package) eq 'ARRAY') {
+		if ($is_bulk_check) {
 			# !!NOT A PART OF THE PUBLIC API!!
 			# Bulk test used by dh to speed up the can_skip check.   It
 			# is NOT useful for finding the most precise pkgfile.
@@ -1115,7 +1154,7 @@ sub default_sourcedir {
 		} else {
 			# Avoid checking for hostarch+hostos unless we have reason
 			# to believe that they exist.
-			if ($check_expensive) {
+			if ($check_expensive and $opts->{'support-architecture-restriction'}) {
 				my $cross_type = uc(package_cross_type($package));
 				push(@try,
 					 "debian/${package}.${filename}.".dpkg_architecture_value("DEB_${cross_type}_ARCH"),
@@ -1189,7 +1228,17 @@ sub isnative {
 	}
 
 	# Make sure we look at the correct changelog.
-	my $isnative_changelog = pkgfile($package, 'changelog', 0);
+	local $dh{NAME};
+	delete($dh{NAME});
+	my $isnative_changelog = pkgfile(
+		{
+			'internal-nameless-variant-handling' => 0,
+			'named'                              => 0,
+			'support-architecture-restriction'   => 0,
+		},
+		$package,
+		'changelog',
+	);
 	if (! $isnative_changelog) {
 		$isnative_changelog = "debian/changelog";
 		$cache_key = '_source';
@@ -2402,7 +2451,14 @@ sub debhelper_script_subst {
 
 	my $tmp=tmpdir($package);
 	my $ext=pkgext($package);
-	my $file=pkgfile($package,$script);
+	my $file = pkgfile(
+		{
+			'named'                              => 0,
+			'support-architecture-restriction'   => 0,
+		},
+		$package,
+		$script,
+	);
 	my %variables = defined($extra_vars) ? %{$extra_vars} : ();
 	my $service_script = generated_file($package, "${script}.service", 0);
 	my @generated_scripts = ("debian/$ext$script.debhelper", $service_script);

.editorconfig
@@ -9,3 +9,6 @@ indent_style = space
 
 [*.{pl,pm}]
 indent_style = tab
+
+[dh_*]
+indent_style = tab

debhelper-compat-upgrade-checklist.pod
@@ -244,6 +244,12 @@ will not have to do anything for this migration.
 
 =item -
 
+The debhelper configuration files are subject to the following changes:
+
+=over 4
+
+=item -
+
 It now triggers a warning to use package-less versions of debhelper configuration
 files when there are 2 or more binary packages listed in F<debian/control> in
 most cases.  Legacy files should be renamed to F<< debian/I<package>.foo >>
@@ -269,6 +275,21 @@ if you had a F<debian/bar.service> with the following snippet in F<debian/rules>
 Then you need to rename F<debian/bar.service> to F<debian/foo.bar.service>.
 
 In compat 15 (or later), this is changed to an error.
+
+=item -
+
+The default look up rules for B<Dh_Lib> based tools now assumes that configuration
+files are no longer named (B<--name>) nor support architecture restrictions by default.
+If you work with a third-party debhelper-like tool and need support for either of these
+features, please file a bug against the tool asking it to declare its configuration
+file with the relevant options in its B<pkgfile> call.
+
+Note that B<debhelper> itself tweaked its rules for most of its tools as well based on
+analysis of usage via B<codesearch.debian.org>. Should you be relying on a feature like
+architecture restrictions for a given config file that is no longer supported, please
+file a feature request for the use-case and it might be restored.
+
+=back
 
 =item -
 

dh_bugfiles
@@ -87,7 +87,14 @@ foreach my $package (@{$dh{DOPACKAGES}}) {
 	# package in question
 	my %bugfiles=();
 	while (my ($type, $pkgfilename) = each(%bugfile_types)) {
-		my $file=pkgfile($package,$pkgfilename);
+		my $file = pkgfile(
+			{
+				'named'                              => 0,
+				'support-architecture-restriction'   => 0,
+			},
+			$package,
+			$pkgfilename,
+		);
 		if ($file) {
 			$bugfiles{$type}=$file;
 		}

dh_compress
@@ -85,7 +85,14 @@ on_pkgs_in_parallel {
 	foreach my $package (@_) {
 		my $tmp=tmpdir($package);
 
-		my $compress=pkgfile($package,"compress");
+		my $compress = pkgfile(
+			{
+				'named'                              => 0,
+				'support-architecture-restriction'   => 0,
+			},
+			$package,
+			"compress",
+		);
 
 		# Run the file name gathering commands from within the directory
 		# structure that will be effected.

dh_gencontrol
@@ -63,7 +63,7 @@ if (not compat(13)) {
 }
 
 # The `substvars` file ius not quite a pkgfile, but it works similar enough that people might
-# be surprised if it was bit detected.
+# be surprised if it was not detected.
 # INTROSPECTABLE: CONFIG-FILES pkgfile(substvars)
 
 on_pkgs_in_parallel {
@@ -75,7 +75,15 @@ on_pkgs_in_parallel {
 
 		my $substvars="debian/${ext}substvars";
 
-		my $changelog=pkgfile($package, 'changelog', 1);
+		my $changelog = pkgfile(
+			{
+				'internal-nameless-variant-handling' => 1,
+				'named'                              => 0,
+				'support-architecture-restriction'   => 0,
+			},
+			$package,
+			'changelog',
+		);
 
 		install_dir("$tmp/DEBIAN");
 

dh_install
@@ -159,7 +159,16 @@ foreach my $package (getpackages()) {
 	my $skip_install = process_pkg($package) ? 0 : 1;
 
 	my $tmp=tmpdir($package);
-	my $file=pkgfile($package,"install");
+	my $file = pkgfile(
+		{
+			'named'                              => 0,
+			# At least, `util-linux` uses `dh-exec` with a `... > debian/foo.install.amd64` as
+			# a work around.
+			'support-architecture-restriction'   => 1,
+		},
+		$package,
+		"install",
+	);
 
 	my @install;
 	if ($file) {

dh_installalternatives
@@ -77,7 +77,14 @@ $dh{NAME} = undef;
 
 foreach my $package (@{$dh{DOPACKAGES}}) {
 	my $tmp = tmpdir($package);
-	my $alternatives = pkgfile($package, 'alternatives');
+	my $alternatives = pkgfile(
+		{
+			'named'                              => 0,
+			'support-architecture-restriction'   => 0,
+		},
+		$package,
+		'alternatives',
+	);
 
 	if (-f $alternatives) {
 		_parse_alternatives_file_and_generate_maintscripts($package, $tmp, $alternatives);

commit 214c2a299dfcc1f0cb79ea4a0e80d67df54be19a

Fix regression in pkgfile where compat 14 semantics were always active
Fixes: a16a181f2f141c8ae6b8101ee530fff7a67cfc78
Thanks: Salvatore Bonaccorso <carnil@debian.org>

lib/Debian/Debhelper/Dh_Lib.pm
@@ -1132,7 +1132,7 @@ sub default_sourcedir {
 		# globally or not.  But if someone is being "clever" then the
 		# cache is reusable and for the general/normal case, it has no
 		# adverse effects.
-		if (defined $dh{NAME} and $opts->{'named'}) {
+		if (defined $dh{NAME} and $named) {
 			$filename="$dh{NAME}.$filename";
 		}
 
@@ -1154,7 +1154,7 @@ sub default_sourcedir {
 		} else {
 			# Avoid checking for hostarch+hostos unless we have reason
 			# to believe that they exist.
-			if ($check_expensive and $opts->{'support-architecture-restriction'}) {
+			if ($check_expensive and $support_architecture_restriction) {
 				my $cross_type = uc(package_cross_type($package));
 				push(@try,
 					 "debian/${package}.${filename}.".dpkg_architecture_value("DEB_${cross_type}_ARCH"),