MichalMaruska/debhelper

commit becd916bb14cbb58eadfc6543366c0e70b5833ab

meson: Refactor a conditional to minize the contents of the branches
Gbp-Dch: ignore

lib/Debian/Debhelper/Buildsystem/meson.pm
@@ -141,11 +141,12 @@ sub test {
 			if ($this->get_parallel() > 0) {
 				$options{update_env}{MESON_TESTTHREADS} = $this->get_parallel();
 			}
-			if (!$dh{QUIET}) {
-				$this->doit_in_builddir(\%options, 'meson', 'test', '--verbose', @_);
-			} else {
-				$this->doit_in_builddir(\%options, 'meson', 'test', @_);
-			}
+			my @meson_cmd = qw(
+				'meson',
+				'test',
+			);
+			push(@meson_cmd, '--verbose') if not $dh{QUIET};
+			$this->doit_in_builddir(\%options, @meson_cmd, @_);
 		}
 	};
 	if (my $err = $@) {

commit e84060d21bc23b002acdd60a6c0e71e2b7cc5c1e

Fix regression when running 'meson test'
This needs to be either ('quoted', 'words') or qw(space separated words):
mixing the two syntaxes isn't going to work.

Fixes: becd916b "meson: Refactor a conditional to minize the contents of the branches"
Closes: #1093436

lib/Debian/Debhelper/Buildsystem/meson.pm
@@ -141,7 +141,7 @@ sub test {
 			if ($this->get_parallel() > 0) {
 				$options{update_env}{MESON_TESTTHREADS} = $this->get_parallel();
 			}
-			my @meson_cmd = qw(
+			my @meson_cmd = (
 				'meson',
 				'test',
 			);